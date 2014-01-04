bfd6e239a2929b141c0922448e8b62d1b85398a9
Gajanan <gchandgadkar@hp.com>
2014-01-04 00:13:21 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbOnRails::Application.config.secret_token = '14d794918bd4ec42a3610a5e0409255b19da102ff03b648a6f48844ccce27a54ec340a6399b052bd18f64a3a353de2f818697c665a6268271d6c47a22dc9186d'
